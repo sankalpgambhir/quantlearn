183153e88f44a52989be53d47aacbb3f93fd90a3
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2020-10-13 00:17:50 +0530

    GalacticBugSquasher OVA

--- src/quantdriver.cc
+++ src/quantdriver.cc
@@ -351,29 +351,29 @@ void Trace::compute_prop_counts(){
 void Trace::compute_not_props(){
     // compute prop optimizations
     std::vector<std::pair<std::string, proposition> > new_inserts;
-    for(auto m : this->prop_inst){
-        auto s = m.first;
-        auto p = m.second;
+    for(auto const &m : this->prop_inst){
+        auto &s = m.first;
+        auto &p = m.second;
 
         proposition np; // create a not prop
         np.name = "~" + s;
         np.instances.clear(); // make sure we're empty
 
-
         auto it = p.instances.begin();
-        if(it == p.instances.end()){
-            continue;
-        }
 
         for(int i = 0; i < this->length; i++){
-            if(((*it).position == i) && (it != p.instances.end())){
-                it++; // else we have reached the end of p, keep adding ~p
-                continue; // ~p doesn't hold here
+            if(it != p.instances.end()){ // else we have reached the end of p, keep adding ~p
+                if(it->position == i){
+                    it++; 
+                    continue; // ~p doesn't hold here
+                }
             }
             np.instances.emplace_back(i); // ~p here
         }
         new_inserts.push_back({np.name, np});
     }
+
+    assert(new_inserts.size() == this->prop_inst.size()); // propositions should double
 
     for(auto &npair : new_inserts){
         this->prop_inst.insert(npair);
